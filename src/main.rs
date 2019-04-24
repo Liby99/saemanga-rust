ee0b3e959c57cf181468f29d98e2f333b45b12c7
Liby Lee <liby99@icloud.com>
2019-04-24 00:41:45 -0700

    Able to connect to mongodb

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
 
 use rocket::Request;
 use rocket::response::Redirect;
 use rocket_contrib::templates::{Template, handlebars};
+use rocket_contrib::databases::mongodb;
 
-#[derive(Serialize)]
-struct IndexTemplateContext {
-  name: &'static str,
-}
+#[database("mongodb")]
+struct SaemangaDatabase(mongodb::db::Database);
 
 #[get("/")]
 fn root() -> Redirect {
   Redirect::to("/index")
+}
+
+#[derive(Serialize)]
+struct IndexTemplateContext {
+  name: &'static str,
 }
 
 #[get("/")]
@@ -32,6 +37,7 @@ fn hello_world() -> &'static str {
 fn main() {
   rocket::ignite()
     .attach(Template::fairing())
+    .attach(SaemangaDatabase::fairing())
     .mount("/", routes![root])
     .mount("/index", routes![index])
     .mount("/hello-world", routes![hello_world])

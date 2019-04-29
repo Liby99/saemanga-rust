a6d1844fa9e8830f59d143fd8acf5e51bc7d1d4e
Liby Lee <liby99@icloud.com>
2019-04-29 01:47:53 -0700

    Compiling manga module

--- src/app/manga.rs
+++ src/app/manga.rs
@@ -1,33 +1,83 @@
-use std::Option;
 use chrono::prelude::*;
 use mongodb::oid::ObjectId;
 
-mod genre;
+use super::genre::*;
 
 pub enum MangaStatus {
   Ended,
   Updating,
 }
 
-pub struct MangaEpisode {
-  episode: u32,
-  num_pages: u32,
-  update_date: DateTime<Utc>
+impl MangaStatus {
+  pub fn ended(&self) -> bool {
+    match self {
+      MangaStatus::Ended => true,
+      _ => false,
+    }
+  }
 }
 
-pub struct MangaBook {
-  book: u32,
+pub struct MangaEpisode {
+  index: u32,
+  is_book: bool,
+  episode: u32,
   num_pages: u32,
+}
+
+impl MangaEpisode {
+  pub fn new(index: u32, is_book: bool, episode: u32, num_pages: u32) -> MangaEpisode {
+    MangaEpisode { 
+      index: index,
+      is_book: is_book,
+      episode: episode, 
+      num_pages: num_pages,
+    }
+  }
+
+  pub fn index(&self) -> u32 {
+    self.index
+  }
+
+  pub fn is_book(&self) -> bool {
+    self.is_book
+  }
+
+  pub fn episode(&self) -> u32 {
+    self.episode
+  }
+
+  pub fn num_pages(&self) -> u32 {
+    self.num_pages
+  }
+}
+
+pub enum MangaDmkIdBase {
+  V10 { dmk_id_web: String, dmk_id_home: String },
+  V09 { dmk_id_home: String },
+  V08 { dmk_id_home: String },
+  V07 { dmk_id_gen: String },
+  V06 { dmk_id_gen: String },
+  V05 { dmk_id_web: String, dmk_id_gen: String },
+}
+
+impl MangaDmkIdBase {
+  pub fn dmk_image_url_base(&self) -> String {
+    match self {
+      MangaDmkIdBase::V10 { dmk_id_web, dmk_id_home } => format!("http://{}.cartoonmad.com/{}", dmk_id_web, dmk_id_home),
+      MangaDmkIdBase::V09 { dmk_id_home } => format!("http://cartoonmad.com/{}", dmk_id_home),
+      MangaDmkIdBase::V08 { dmk_id_home } => format!("http://cartoonmad.com/{}", dmk_id_home),
+      MangaDmkIdBase::V07 { dmk_id_gen } => format!("http://www.cartoonmad.com/home1/{}", dmk_id_gen),
+      MangaDmkIdBase::V06 { dmk_id_gen } => format!("http://www.cartoonmad.com/cartoonimg/{}", dmk_id_gen),
+      MangaDmkIdBase::V05 { dmk_id_web, dmk_id_gen } => format!("http://{}.cartoonmad.com/{}", dmk_id_web, dmk_id_gen),
+    }
+  }
 }
 
 pub struct Manga {
 
   // dmk information
-  dmk_id_ver: u8,
-  dmk_id: MangaUrlComponent,
-  dmk_id_web: Option<String>,
-  dmk_id_home: Option<String>,
-  dmk_id_gen: Option<String>,
+  dmk_id: String,
+  dmk_id_base: MangaDmkIdBase,
 
   // Information of the manga
   title: String,
@@ -38,7 +88,6 @@ pub struct Manga {
   status: MangaStatus,
 
   // Episode Information
-  books: Vec<Mangabook>,
   episodes: Vec<MangaEpisode>,
 
   // Other Information
@@ -49,51 +98,66 @@ pub struct Manga {
 
 impl Manga {
   pub fn title(&self) -> &String {
-    self.title
+    &self.title
   }
 
   pub fn description(&self) -> &String {
-    self.description
+    &self.description
   }
 
   pub fn author(&self) -> &String {
-    self.author
+    &self.author
   }
 
   pub fn tags(&self) -> &Vec<String> {
-    self.tags
+    &self.tags
   }
 
   pub fn genre(&self) -> &Genre {
-    self.genre
+    &self.genre
   }
 
-  pub fn saemanga_base_url(&self) -> String {
-
+  pub fn status(&self) -> &MangaStatus {
+    &self.status
   }
 
-  pub fn saemanga_url(&self, episode: u32) -> String {
+  pub fn has_book(&self) -> bool {
+    self.episodes.len() > 0 && self.episodes[0].is_book
+  }
 
+  pub fn books(&self) -> Vec<&MangaEpisode> {
+    self.episodes.iter().filter(|e| e.is_book).collect()
+  }
+
+  pub fn episodes(&self) -> Vec<&MangaEpisode> {
+    self.episodes.iter().filter(|e| !e.is_book).collect()
+  }
+
+  pub fn find_episode(&self, epi: u32) -> Option<&MangaEpisode> {
+    self.episodes.iter().find(|&e| e.episode == epi)
+  }
+
+  // pub fn next_episode_of(&self, epi: &MangaEpisoe) -> Option<&MangaEpisode>
+
+  // pub fn prev_episode_of(&self, epi: &MangaEpisoe) -> Option<&MangaEpisode>
+
+  pub fn saemanga_url(&self) -> String {
+    format!("http://saemanga.com/manga/{}", self.dmk_id)
+  }
+
+  pub fn saemanga_episode_url(&self, episode: u32) -> String {
+    format!("http://saemanga.com/manga/{}/{}", self.dmk_id, episode)
   }
 
   pub fn dmk_base_url(&self) -> String {
-
+    format!("https://cartoonmad.com/comic/{}.html", self.dmk_id)
   }
 
-  pub fn dmk_episode_url(&self, episode: u32) -> String {
-
+  pub fn dmk_cover_url(&self) -> String {
+    format!("http://cartoonmad.com/cartoonimg/coimg/{}.jpg", self.dmk_id)
   }
 
   pub fn dmk_image_url(&self, episode: u32, page: u32) -> String {
-
+    format!("{}/{}/{:03}/{:03}.jpg", self.dmk_id_base.dmk_image_url_base(), self.dmk_id, episode, page)
   }
 }
-
-// pub enum MangaUrlComponent {
-//   One { dmk_id_web: String, dmk_id_gen: String },
-//   Two { dmk_id_gen: String },
-//   Three { dmk_id_gen: String },
-//   Four { dmk_id_home: String },
-//   Five { dmk_id_home: String },
-//   Six { dmk_id_web: String, dmk_id_home: String },
-// }
--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,2 +1,3 @@
 pub mod genre;
+pub mod manga;
 pub mod user_setting;

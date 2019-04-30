fb666167d60e73b05012b485d817dff2a468018d
Liby Lee <liby99@icloud.com>
2019-04-30 12:51:52 -0700

    Add lots of templates

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,10 @@
 
 extern crate lazy_static;
 
+use rocket::fairing::Fairing;
 use rocket_contrib::serve::StaticFiles;
-use rocket_contrib::templates::{Template};
+use rocket_contrib::templates::{Template, handlebars};
+use handlebars::{Helper, Handlebars, Context, RenderContext, Output, HelperResult, JsonRender};
 
 pub mod app;
 pub mod api;
@@ -18,6 +20,29 @@ pub mod routes;
 #[database("mongodb")]
 struct Database(mongodb::db::Database);
 
+fn concatenate_helper(
+  h: &Helper,
+  _: &Handlebars,
+  _: &Context,
+  _: &mut RenderContext,
+  out: &mut Output
+) -> HelperResult {
+  for param in h.params() {
+    out.write(&param.value().render())?;
+  }
+  Ok(())
+}
+
+fn template() -> impl Fairing {
+  Template::custom(|engines| {
+    engines.handlebars.register_helper("concat", Box::new(concatenate_helper));
+  })
+}
+
+fn database() -> impl Fairing {
+  Database::fairing()
+}
+
 pub fn launch() {
 
   // Variables
@@ -25,8 +50,8 @@ pub fn launch() {
 
   // Start the server
   rocket::ignite()
-    .attach(Template::fairing())
-    .attach(Database::fairing())
+    .attach(template())
+    .attach(database())
     .mount("/", routes::routes())
     .mount("/", StaticFiles::from(public_path))
     .register(routes::catchers())

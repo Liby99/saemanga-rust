8a6d925439ba1c85e12ccafb36911788071566af
Liby Lee <liby99@icloud.com>
2019-04-30 00:34:17 -0700

    Improved webpack infra

--- assets/typescript/index.ts
+++ assets/typescript/index.ts
@@ -1,6 +1,8 @@
 'use strict';
 
-import "../stylesheets/index.scss"
+import "../images/loading.svg";
+
+import "../stylesheets/index.scss";
 
 import $ = require('jquery');
 
--- webpack.config.js
+++ webpack.config.js
@@ -13,26 +13,43 @@ module.exports = {
     filename: 'js/main.js'
   },
   module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: { loader: 'ts-loader' }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ]
-      },
-    ]
+    rules: [{
+      test: /\.ts$/,
+      exclude: /node_modules/,
+      use: { loader: 'ts-loader' }
+    }, {
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader',
+      ]
+    }, {
+      test: /images\/.+\.(svg|png|jpg|jpeg|gif)/,
+      use: [{
+        loader: "file-loader",
+        options: {
+          name: 'img/[name].[ext]'
+        }
+      }]
+    }, {
+      test: /\.(svg|woff|woff2|ttf|eot)$/,
+      exclude: /images/,
+      use: [{
+        loader: "file-loader",
+        options: {
+          publicPath: '../',
+          name: 'fonts/[name].[ext]'
+        }
+      }]
+    }]
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/main.css"
     })
   ],
-  resolve: { extensions: ['.ts'] }
+  resolve: {
+    extensions: ['.ts']
+  }
 };

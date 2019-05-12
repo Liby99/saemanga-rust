41889893cd4a9afad1587443ae839b396a8b2ecb
Liby Lee <liby99@icloud.com>
2019-05-11 18:28:57 -0700

    Fix lint fix

--- assets/typescript/templates/discover_manga.ts
+++ assets/typescript/templates/discover_manga.ts
@@ -2,10 +2,10 @@ import hbs from '../../templates/partials/index/discover_manga.html.hbs';
 import Template from '../library/template';
 
 export type DiscoverMangaData = {
-  title: string,
-  dmk_id: string,
-  cover_url: string,
-  saemanga_url: string,
+  "title": string,
+  "dmk_id": string,
+  "cover_url": string,
+  "saemanga_url": string,
 };
 
 export class DiscoverMangaTemplate extends Template<DiscoverMangaData> {

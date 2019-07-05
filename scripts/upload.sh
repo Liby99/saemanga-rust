e48f3a2168ab34b13be2af19ca6b5198003a4587
Liby Lee <liby99@icloud.com>
2019-07-05 17:10:02 -0400

    Add comment to upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-echo "Server address? "
+# To run this script, starting from saemanga-rust root directory and type
+#  $ ./scripts/upload.sh
+# Any type in the server address
+#
+# This command will not create a specific folder so please include a folder name
+# e.g. user@server.com:/path/to/saemanga-rust/
+#
+# Also please first do the front-end compilation in PRODUCTION mode and then
+# do the upload
+
+echo "Server address? (e.g. user@server.com:~/path/to/saemanga-rust)"
 
 read addr
 

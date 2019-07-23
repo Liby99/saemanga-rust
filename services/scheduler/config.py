b3ac206d73fa572661ba8c511d85b05617b0580e
Liby Lee <liby99@icloud.com>
2019-07-23 11:45:01 -0400

    Done prod run scripting

--- services/scheduler/config.py
+++ services/scheduler/config.py
@@ -2,12 +2,6 @@ from typing import Dict, Any
 
 def config(prod: bool) -> Dict[str, Any]:
   if prod:
-    return {
-      "addr": "localhost",
-      "port": 8193
-    }
+    return { "addr": "localhost", "port": 8200 }
   else:
-    return {
-      "addr": "localhost",
-      "port": 8000
-    }
+    return { "addr": "localhost", "port": 8000 }
--- services/scheduler/main.py
+++ services/scheduler/main.py
@@ -1,10 +1,20 @@
 import requests
 import sched
 import time
+import sys
 
 from account import account
 from config import config
 from tasks import tasks
+
+args = sys.argv[1:]
+delay = False
+prod = False
+for arg in args:
+  if arg == "--delay":
+    delay = True
+  if arg == "--prod":
+    prod = True
 
 def login(conf, acct):
   url = f"http://{conf['addr']}:{conf['port']}/user/login"
@@ -16,7 +26,7 @@ def periodic(scheduler, interval, action, actionargs=()):
   action(*actionargs)
 
 def main():
-  conf = config(False)
+  conf = config(prod)
   acct = account()
 
   session = login(conf, acct)
@@ -31,4 +41,6 @@ def main():
   scheduler.run()
 
 if __name__ == "__main__":
+  if delay:
+    time.sleep(1000)
   main()

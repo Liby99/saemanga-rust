e13dcf1fb77be435bbb1b5654eb55b9d91e79426
Liby Lee <liby99@icloud.com>
2019-07-23 20:24:42 -0400

    Async manga fetching

--- services/migrate/manga.py
+++ services/migrate/manga.py
@@ -1,32 +1,49 @@
-import datetime
-import subprocess
-import json
+import datetime, subprocess, threading, json
 
+from async_call import Async
 from util import print_progress
 
 def migrate_mangas(old_db, new_db):
+
+  print("Migrating Mangas")
+
   old_manga_coll = old_db["manga"]
   new_manga_coll = new_db["manga"]
   old_mangas = old_manga_coll.find()
   total_manga_count = old_manga_coll.count_documents({})
   completed_count = 0
+  processed_count = 0
   skipped_count = 0
   failed_dmk_ids = []
   manga_migrates = {}
+  threads = []
+
+  # Spawn all the threads
   for old_manga in old_mangas:
-    result = migrate_manga(old_manga, new_manga_coll)
-    completed_count += 1
-    if "failed" in result:
+    thread = migrate_manga(old_manga, new_manga_coll)
+    threads.append(thread)
+    print_progress(len(threads), total_manga_count, prefix="Spawning threads...")
+
+  # Join all the threads and get the result
+  for thread in threads:
+    result = thread.wait()
+    processed_count += 1
+    if result["failed"]:
       failed_dmk_ids.append(old_manga["dmk_id"])
     else:
       if result["existed"]:
         skipped_count += 1
+      completed_count += 1
       manga_migrates[old_manga["_id"]] = result
     print_progress(completed_count, total_manga_count, prefix="Migrating mangas...")
-  failed_count = len(failed_dmk_ids)
-  print(f"Completed count: {completed_count - failed_count}, Skipped count: {skipped_count}")
-  print(failed_dmk_ids)
 
+  failed_count = len(failed_dmk_ids)
+  print(f"Total: {total_manga_count}, Completed: {completed_count}, Skipped: {skipped_count}, Failed: {failed_count}")
+  if failed_count > 0:
+    print(f"Failed dmk ids: {failed_dmk_ids}")
+  return manga_migrates
+
+@Async
 def migrate_manga(old_manga, new_manga_coll):
   dmk_id = old_manga["dmk_id"]
   new_manga = new_manga_coll.find_one({ "dmk_id": dmk_id })
@@ -38,8 +55,9 @@ def migrate_manga(old_manga, new_manga_coll):
       new_manga = fetch_new_manga(old_manga)
       result = new_manga_coll.insert_one(new_manga)
       return { "_id": result.inserted_id, "dmk_id": dmk_id, "existed": False, "failed": False }
-    except:
-      return { "failed": True }
+    except Exception as err:
+      print(err)
+      return { "dmk_id": dmk_id, "failed": True }
 
 def fetch_new_manga(old_manga):
   dmk_id = old_manga["dmk_id"]
